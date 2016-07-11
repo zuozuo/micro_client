7147d6a3897cc0090f17fa81646f6e8ced188832
zuozuo <c_yzuo@groupon.com>
2016-07-11 09:54:45 +0800

    engine to config

--- lib/micro_client/config.rb
+++ lib/micro_client/config.rb
@@ -3,7 +3,7 @@ module MicroClient
     attr_accessor :service_name, :service_address
 
     def get_service
-      raise 'This method should be overrided by subclass of MicroClient::Engine'
+      raise 'This method should be overrided by subclass of MicroClient::Config'
     end
   end
 end
